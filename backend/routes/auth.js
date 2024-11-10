630d5020c738297945a7baf2755a936105933711
unknown <durgaballepod07@gmail.com>
2024-11-10 12:27:29 +0530

    Normalize line endings

---
+++ backend/routes/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Authentication token is required" });
+    }
+
+    jwt.verify(token, "Pammu@143", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid token" });
+        }
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
